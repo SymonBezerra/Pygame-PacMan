be6ca653dc9f29ca821eef3d51800c255cddd0c6
SymonBezerra <simaobezerratranslate@gmail.com>
2022-12-31 18:55:44 -0300

    first commit

---
+++ wall.py
@@ -0,0 +1,13 @@
+import pygame
+
+WALL_SPRITES = {1: "gfx/wall_vertical"}
+
+class Wall (pygame.sprite.Sprite):
+
+    def __init__ (self, coordinate: tuple, 
+    type: int) -> pygame.sprite.Sprite:
+        self.coordinate = coordinate
+        
+        self.image = pygame.image.load(WALL_SPRITES[type]).convert_alpha()
+        self.image = pygame.transform.scale((50, 50))
+        self.rect = self.image.get_rect(center=(coordinate))
