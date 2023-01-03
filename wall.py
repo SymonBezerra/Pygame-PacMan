1a8866cdb5e794766a0430e97f61f149c29a18f1
SymonBezerra <simaobezerratranslate@gmail.com>
2023-01-02 22:01:06 -0300

    added special impl

--- wall.py
+++ wall.py
@@ -3,7 +3,8 @@ import pygame
 WALL_SIZE = 25
 
 WALL_SPRITES = {0: "gfx/food_sprite.png",
-1: "gfx/wall_sprite.png"}
+1: "gfx/wall_sprite.png",
+2: "gfx/special_sprite.png"}
 
 class Wall (pygame.sprite.Sprite):
 
